a69b8526afa899e105b7170f2e3ed050d11c93a1
Juanmi <Juanmi26304@gmail.com>
2024-10-12 15:27:22 +0200

    Exploding-Cards
    CSS not finished but Client task completely finished

---
+++ javaScript/card.js
@@ -0,0 +1,9 @@
+export class Card {
+  name;
+  value;
+
+  constructor(name, value = null) {
+    this.name = name;
+    this.value = value;
+  }
+};
---
+++ javaScript/constants.js
@@ -0,0 +1,16 @@
+export const TOTAL_CARDS = 60;
+export const BOMBS_NUMBER = 4;
+export const DEFUSES_NUMBER = 6;
+export const SKIPSTURNS_NUMBER = 10;
+export const NOPES_NUMBER = 5;
+export const POINTS_NUMBER = 35;
+export const MIN_POINTS = 1;
+export const MAX_POINTS = 10;
+export const CARD_TYPES_NUMBER = 5;
+export const CARD_TYPES = {
+  BOMB: "Bomb",
+  DEFUSE: "Defuse",
+  SKIPTURN: "Jump",
+  NOPE: "Nope",
+  POINTS: "Points"
+};
